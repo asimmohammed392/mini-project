27ccd01fb4bebe3b2c4a5091d42bd29a9e1dfa77
mohammedsohail-hacker <79409236+mohammedsohail-hacker@users.noreply.github.com>
2021-10-06 13:39:51 +0530

    Update quiz.js

--- JS Files/quiz.js
+++ JS Files/quiz.js
@@ -1,6 +1,6 @@
 function check(){
     var count=0;
-    var q1=document.quiz.q1.value;
+    var q1=document.quiz.ques1.value;
     var q2=document.quiz.q2.value;
     var q3=document.quiz.q3.value;
     var q4=document.quiz.q4.value;
