dd7785f54437ab248c9c414087a341e404ed6102
mohammedasim29 <asimmohammed392@gmail.com>
2021-10-07 12:02:59 +0530

    quiz_template styled a little

---
+++ JS Files/quiz_js.js
@@ -0,0 +1,15 @@
+function check(){
+    var count=0;
+    var q1=document.quiz.q1.value;
+    var q2=document.quiz.q2.value;
+    var q3=document.quiz.q3.value;
+    var q4=document.quiz.q4.value;
+    var q5=document.quiz.q5.value;
+    if(q1=="Java"){count++}
+    if(q2=="Russia"){count++}
+    if(q3=="Sohail"){count++}
+    if(q4=="Phir Hera pheri"){count++}
+    if(q5=="Akshay"){count++}
+    
+    document.write('<h1 class="result">'+"Your Score is "+((count/5)*100)+"%"+'</h1>');
+}
